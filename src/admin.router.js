a03f3d1e33e93e00ff19f565203f2aa8b14d8d5f
Ramamoorthy P <ramamoorthyp@gmail.com>
2021-12-13 10:06:04 +0530

    removed unused vars

--- src/admin.router.js
+++ src/admin.router.js
@@ -1,6 +1,4 @@
-const AdminBro = require('admin-bro');
 const { buildRouter } = require('admin-bro-expressjs');
-const express = require('express');
 
 const buildAdminRouter = (admin) => buildRouter(admin);
 
--- src/user.entity.js
+++ src/user.entity.js
@@ -14,4 +14,4 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', UserSchema);
+mongoose.model('User', UserSchema);
